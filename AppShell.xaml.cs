c005e087a6067ef591297e444adee9638b21986e
Eugene Novikov <eugene.novikov@devexpress.com>
2022-05-02 17:12:43 +0400

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BugInTabbedUIMoreItems;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage2.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace BugInTabbedUIMoreItems;
+
+public partial class MainPage2 : ContentPage
+{
+	int count = 0;
+
+	public MainPage2()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
